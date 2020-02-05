7ab239fba42dfdf108358a25c70e95108d877a14
mkulacha <murali.kulachandran@traveledge.com>
2020-02-05 11:07:47 -0500

    Built angular with ng and added Angular Material

--- KTInsta/Startup.cs
+++ KTInsta/Startup.cs
@@ -24,7 +24,7 @@ namespace KTInsta
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "KTInsta/dist";
             });
         }
 
@@ -55,7 +55,7 @@ namespace KTInsta
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "KTInsta";
 
                 if (env.IsDevelopment())
                 {
